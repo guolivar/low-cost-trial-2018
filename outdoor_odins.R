bccbb8f4d37ffd018013673f7a0632f3d3ef2485
Gustavo Olivares <1390863+guolivar@users.noreply.github.com>
2019-04-04 21:30:00 +0000

    Done fetching WEB ODIN data

--- outdoor_odins.R
+++ outdoor_odins.R
@@ -25,7 +25,9 @@ shelf(readr,
       dplyr,
       RColorBrewer,
       purrr,
-      magick)
+      magick,
+      foreach,
+      doParallel)
 
 # Read the secrets
 secret_hologram <- read_delim("./secret_hologram.txt", 
@@ -47,11 +49,24 @@ for (i in (1:ntags)){
 }
 wanted_tags_human <- c("waterview_outdoor","waterview","waterview_indoor")
 tags <- subset(all_tags,name %in% wanted_tags_human)
-wanted_tags <-tags$topic[1]
-for (i in (2:length(tags))){
-  wanted_tags <- paste0(wanted_tags,",",tags$topic[i])
-}
+wanted_tags <-paste(tags$topic,collapse = ",")
+print(wanted_tags)
 
+# Fetch the ODIN names
+base_url <- "https://dashboard.hologram.io/api/1/devices?"
+built_url <- paste0(base_url,
+                    "limit=500&",
+                    "orgid=",secret_hologram$orgid,"&",
+                    "apikey=",secret_hologram$apikey)
+req1 <- curl_fetch_memory(built_url)
+jreq1 <- fromJSON(rawToChar(req1$content))$data
+ndevices <- length(jreq1)
+all_devices <- data.frame(id = (1:ndevices),name = NA)
+
+for (i in (1:ndevices)){
+  all_devices$id[i] <- jreq1[[i]]$id
+  all_devices$name[i] <- jreq1[[i]]$name
+}
 
 ## Get the timeseries data #####
 # UTC time start
@@ -60,7 +75,7 @@ print(x_now)
 # UTC time start
 t_start <- as.numeric(as.POSIXct("2018/10/29 12:00:00",tz = "GMT-12"))
 # UTC time end ... now
-t_end <- floor(as.numeric(x_now))
+t_end <- as.numeric(as.POSIXct("2019/03/01 12:00:00",tz = "GMT-12"))
 # Set the averaging interval
 time_avg <- '15 min'
 
@@ -71,6 +86,8 @@ base_url <- "https://dashboard.hologram.io/api/1/csr/rdm?"
 
 while (ndata >= 1){
   if (nstep == 1){
+    print("First 1000 fetch")
+    print(nstep)
     built_url <- paste0(base_url,
                         "topicnames=",wanted_tags,"&",
                         "timestart=",t_start,"&",
@@ -82,6 +99,8 @@ while (ndata >= 1){
     jreq2_tmp <- fromJSON(rawToChar(req2$content))$data
     jreq2 <- jreq2_tmp
   } else {
+    print("Next 1000 fetch")
+    print(nstep)
     built_url <- paste0(base_url,
                         "topicnames=",wanted_tags,"&",
                         "timestart=",t_start,"&",
@@ -104,95 +123,117 @@ while (ndata >= 1){
   print(jreq2_tmp[[ndata]]$logged)
 }
 
+
+
+
 ndata <- length(jreq2)
 print("Got data")
 
-c_data <- data.frame(id = (1:ndata))
-c_data$PM1 <- NA
-c_data$PM2.5 <- NA
-c_data$PM10 <- NA
-c_data$PMc <- NA
-c_data$GAS1 <- NA
-c_data$Tgas1 <- NA
-c_data$GAS2 <- NA
-c_data$Temperature <- NA
-c_data$RH <- NA
-c_data$date <- as.POSIXct(jreq2[[ndata]]$logged,format = "%Y-%m-%d %H:%M:%OS",tz="UTC")
-c_data$timestamp <- c_data$date
-c_data$serialn <- curr_data$ODIN[i_dev]
+# We'll do this in parallel because it takes A LONG time with a few 100k records
+#setup parallel backend to use many processors
+cores <- detectCores()
+cl <- makeCluster(cores[1]-3) #not to overload your computer
+registerDoParallel(cl)
 
-for (i in (1:ndata)){
+all_data <- foreach(i=1:ndata,.packages=c("base64enc","RJSONIO"),.combine=rbind) %dopar%
+{
+  c_data <- data.frame(id = 1)
+  c_data$PM1 <- NA
+  c_data$PM2.5 <- NA
+  c_data$PM10 <- NA
+  c_data$PMc <- NA
+  c_data$GAS1 <- NA
+  c_data$Tgas1 <- NA
+  c_data$GAS2 <- NA
+  c_data$Temperature <- NA
+  c_data$RH <- NA
+  c_data$date <- NA
+  c_data$timestamp <- NA
+  c_data$deviceid <- NA
+  c_data$tags <- NA
   xxx <- rawToChar(base64decode(fromJSON(jreq2[[i]]$data)$data))
   x_payload <- try(fromJSON(xxx),silent = TRUE)
   if (inherits(x_payload,"try-error")) {
+    c_data
     next
   }
-  
   payload <- unlist(x_payload)
   if (length(payload)<5){
+    c_data
     next
   }
   # {"PM1":4,"PM2.5":6,"PM10":6,"GAS1":-999,"Tgas1":0,"GAS2":204,"Temperature":7.35,"RH":80.85,"recordtime":"2018/07/11;00:21:01"}
-  c_data$PM1[i] <- as.numeric(payload[1])
-  c_data$PM2.5[i] <- as.numeric(payload[2])
-  c_data$PM10[i] <- as.numeric(payload[3])
-  c_data$PMc[i] <- as.numeric(payload[3]) - as.numeric(payload[2])
-  c_data$GAS1[i] <- as.numeric(payload[4])
-  c_data$Tgas1[i] <- as.numeric(payload[5])
-  c_data$GAS2[i] <- as.numeric(payload[6])
-  c_data$Temperature[i] <- as.numeric(payload[7])
-  c_data$RH[i] <- as.numeric(payload[8])
-  c_data$date[i] <- as.POSIXct(as.character(payload[9]),format = "%Y/%m/%d;%H:%M:%S",tz="UTC")
-  c_data$timestamp[i] <- as.POSIXct(jreq2[[i]]$logged,format = "%Y-%m-%d %H:%M:%OS",tz="UTC")
+  c_data$PM1 <- as.numeric(payload[1])
+  c_data$PM2.5 <- as.numeric(payload[2])
+  c_data$PM10 <- as.numeric(payload[3])
+  c_data$PMc <- as.numeric(payload[3]) - as.numeric(payload[2])
+  c_data$GAS1 <- as.numeric(payload[4])
+  c_data$Tgas1 <- as.numeric(payload[5])
+  c_data$GAS2 <- as.numeric(payload[6])
+  c_data$Temperature <- as.numeric(payload[7])
+  c_data$RH <- as.numeric(payload[8])
+  c_data$date <- as.POSIXct(as.character(payload[9]),format = "%Y/%m/%d;%H:%M:%S",tz="UTC")
+  c_data$timestamp <- as.POSIXct(jreq2[[i]]$logged,format = "%Y-%m-%d %H:%M:%OS",tz="UTC")
+  c_data$deviceid <- jreq2[[i]]$deviceid
+  c_data$tags <- paste((jreq2[[i]]$tags),collapse = ",")
+  c_data
 }
 
-has_data <- try(length(c_data$PM1),silent = TRUE)
-if (inherits(has_data,"try-error")) {
-  next
+all_data$serialn <- NA
+device_ids <- unique(all_data$deviceid)
+for (i in device_ids){
+  all_data$serialn[all_data$deviceid==i] <- subset(all_devices,id==i)$name
 }
-print(min(c_data$timestamp))
-print(max(c_data$timestamp))
-wrong_dates <- which(is.na(c_data$date) | (c_data$date <= as.POSIXct("2018/01/01")) | c_data$date > as.POSIXct(Sys.time()))
-tmp_error_catching <- try(c_data$date[wrong_dates] <- c_data$timestamp[wrong_dates],
+
+print(min(all_data$timestamp))
+print(max(all_data$timestamp))
+names(all_data)
+
+wrong_dates <- which(is.na(all_data$date) | (all_data$date <= as.POSIXct("2018/01/01")) | all_data$date > as.POSIXct(Sys.time()))
+tmp_error_catching <- try(all_data$date[wrong_dates] <- all_data$timestamp[wrong_dates],
                           silent = TRUE)
-wrong_dates <- which(c_data$date <= as.POSIXct("2010/01/01"))
-tmp_error_catching <- try(c_data$date[wrong_dates] <- NA,
+wrong_dates <- which(all_data$date <= as.POSIXct("2010/01/01"))
+tmp_error_catching <- try(all_data$date[wrong_dates] <- NA,
                           silent = TRUE)
-print(min(c_data$date))
-print(max(c_data$date))
 
-# wide data
-wide_data <- merge(subset(all_data,serialn == curr_data$ODIN[x_dev[1]]),
-                   subset(all_data,serialn == curr_data$ODIN[x_dev[2]]),
-                   by = "date",
-                   all=TRUE)
-names(wide_data) <- c('date',
-                      paste0(names(all_data)[c(1:10,12:13)],'.',substr(curr_data$ODIN[1],6,9)),
-                      paste0(names(all_data)[c(1:10,12:13)],'.',substr(curr_data$ODIN[2],6,9)))
-for (i_dev in (3:nsites)) {
-  c_names <- names(wide_data)
-  wide_data <- merge(wide_data,
-                     subset(all_data,serialn == curr_data$ODIN[i_dev]),
-                     by = "date",
-                     all=TRUE)
-  names(wide_data) <- c(c_names,
-                        paste0(names(all_data)[c(1:10,12:13)],'.',substr(curr_data$ODIN[i_dev],6,9)))
-}
+ggplot(data = all_data,aes(x=date)) +
+  geom_line(aes(y = PM10,colour = serialn)) +
+  ylim(0,120)
 
+save(all_data,all_devices,all_tags,file = './odin_hologram.RData')
 
-# Plot data ####
-avg_plot <- '10 min'
-for (i_dev in (1:nsites)) {
-  timePlot(subset(all_data,serialn == curr_data$ODIN[i_dev]),
-           pollutant = c('PM1','PM2.5','PM10'),
-           main = curr_data$ODIN[i_dev],
-           avg.time = avg_plot,
-           group = TRUE,
-           ylim=c(0,30))
-}
-
-timeVariation(subset(all_data,serialn == "ODIN-0072 (89772)"),
-              pollutant = c('PM10','PM2.5','PM1'))
+# # wide data
+# wide_data <- merge(subset(all_data,serialn == curr_data$ODIN[x_dev[1]]),
+#                    subset(all_data,serialn == curr_data$ODIN[x_dev[2]]),
+#                    by = "date",
+#                    all=TRUE)
+# names(wide_data) <- c('date',
+#                       paste0(names(all_data)[c(1:10,12:13)],'.',substr(curr_data$ODIN[1],6,9)),
+#                       paste0(names(all_data)[c(1:10,12:13)],'.',substr(curr_data$ODIN[2],6,9)))
+# for (i_dev in (3:nsites)) {
+#   c_names <- names(wide_data)
+#   wide_data <- merge(wide_data,
+#                      subset(all_data,serialn == curr_data$ODIN[i_dev]),
+#                      by = "date",
+#                      all=TRUE)
+#   names(wide_data) <- c(c_names,
+#                         paste0(names(all_data)[c(1:10,12:13)],'.',substr(curr_data$ODIN[i_dev],6,9)))
+# }
+# 
+# 
+# # Plot data ####
+# avg_plot <- '10 min'
+# for (i_dev in (1:nsites)) {
+#   timePlot(subset(all_data,serialn == curr_data$ODIN[i_dev]),
+#            pollutant = c('PM1','PM2.5','PM10'),
+#            main = curr_data$ODIN[i_dev],
+#            avg.time = avg_plot,
+#            group = TRUE,
+#            ylim=c(0,30))
+# }
+# 
+# timeVariation(subset(all_data,serialn == "ODIN-0072 (89772)"),
+#               pollutant = c('PM10','PM2.5','PM1'))
 # 
 # timePlot(subset(wide_data,date > as.POSIXct("2018-08-27 21:00:00",tz = 'GMT')),
 #          pollutant = c(paste0('Temperature.',odin1),paste0('Temperature.',odin2)),
