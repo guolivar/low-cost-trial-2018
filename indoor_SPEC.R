f65b9445a65415382f7f795819a3e3ec37df5c41
Gustavo Olivares <gustavo@niwa-30431.niwa.local>
2019-10-17 14:21:23 +1300

    Some work with SPEC data

--- indoor_SPEC.R
+++ indoor_SPEC.R
@@ -27,8 +27,6 @@ library(RColorBrewer)
 library(purrr)
 library(magick)
 
-# Read key
-read_address <- "http://penap-data.dyndns.org:8080/output/4mWeal6PZVSlE1kyNkQdCvVadDE.json"
 
 # Load API data
 data.api <- read_delim("~/data/Waterview2018/APINOx/tunnel/LOF01.txt", 
@@ -41,34 +39,48 @@ data.api$date <- start_date + idx_vec
 
 data.api.1min <- timeAverage(data.api,avg.time = '1 min')
 
-## Get the timeseries data #####
-# Get the data #####
-req1 <- curl_fetch_memory(read_address)
-jreq1 <- fromJSON(rawToChar(req1$content))
-
-# UTC time start
-x_now <- Sys.time()
-print(x_now)
-# UTC time start
-t_start <- (as.POSIXct("2018/11/12 00:00:00",tz = "GMT-12"))
-# UTC time end
-t_end <- (as.POSIXct("2018/12/01 20:00:00",tz = "GMT-12"))
-# Set the averaging interval
-time_avg <- '15 min'
-
-xx2 <- unlist(jreq1)
-ndata <- length(xx2)
-data.spec <- data.frame(date = as.POSIXct(xx2[seq(7,ndata,7)],format="%Y-%m-%dT%H:%M:%OS", tz='UTC'))
-data.spec$CO <- as.numeric(xx2[seq(1,ndata,7)])
-data.spec$T_CO <- as.numeric(xx2[seq(2,ndata,7)])
-data.spec$NO2 <- as.numeric(xx2[seq(3,ndata,7)])
-data.spec$T_NO2 <- as.numeric(xx2[seq(4,ndata,7)])
-data.spec$SN_CO <- as.numeric(xx2[seq(5,ndata,7)])
-data.spec$SN_NO2 <- as.numeric(xx2[seq(6,ndata,7)])
-data.spec$CO.NO2 <- data.spec$CO/data.spec$NO2
-
-data.spec <- subset(data.spec,(date>t_start)&(date < t_end))
-
+## Get the SPEC data #####
+# CHOOSE IF GETTING ONLINE DATA
+go_online <- FALSE
+if (go_online){
+  # Fetch from PHANT server
+  req1 <- curl_fetch_memory(read_address)
+  jreq1 <- fromJSON(rawToChar(req1$content))
+  
+  # UTC time start
+  x_now <- Sys.time()
+  print(x_now)
+  # UTC time start
+  t_start <- (as.POSIXct("2018/11/12 00:00:00",tz = "GMT-12"))
+  # UTC time end
+  t_end <- (as.POSIXct("2018/12/01 20:00:00",tz = "GMT-12"))
+  # Set the averaging interval
+  time_avg <- '15 min'
+  
+  xx2 <- unlist(jreq1)
+  ndata <- length(xx2)
+  data.spec <- data.frame(date = as.POSIXct(xx2[seq(7,ndata,7)],format="%Y-%m-%dT%H:%M:%OS", tz='UTC'))
+  data.spec$CO <- as.numeric(xx2[seq(1,ndata,7)])
+  data.spec$T_CO <- as.numeric(xx2[seq(2,ndata,7)])
+  data.spec$NO2 <- as.numeric(xx2[seq(3,ndata,7)])
+  data.spec$T_NO2 <- as.numeric(xx2[seq(4,ndata,7)])
+  data.spec$SN_CO <- as.numeric(xx2[seq(5,ndata,7)])
+  data.spec$SN_NO2 <- as.numeric(xx2[seq(6,ndata,7)])
+  data.spec$CO.NO2 <- data.spec$CO/data.spec$NO2
+  
+  data.spec <- subset(data.spec,(date>t_start)&(date < t_end))
+} else {
+  #Load from local file
+  data.spec.raw <- read_csv("/mnt/bigdisk/data/Waterview2018/SPEC/tunnel-data.txt")
+  data.spec.raw$date <- as.POSIXct(data.spec.raw$timestamp,tz='UTC')
+  data.spec <- data.spec.raw[,c('date','ppb1','adc1','ppb2','adc2')]
+  names(data.spec) <- c('date',
+                        'COspec',
+                        'COspecRAW',
+                        'NO2spec',
+                        'NO2specRAW')
+  
+}
 #### Merge data ####
 names(data.spec) <- c('date','COspec','T_CO','NO2spec','T_NO2','SN_CO','SN_NO2','CO.NO2')
 names(data.api.1min) <- c('date','NOx','NO2raw','NO')
